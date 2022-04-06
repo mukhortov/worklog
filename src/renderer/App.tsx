ae225503d982cd6771f8dffac9a9cd530125912a
Peter Mukhortov <peter@mukhortov.com>
2022-04-06 22:12:58 +0300

    Use sass files and css modules

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,15 +1,15 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
-import './App.css';
+import styles from './App.sass';
 
 const Hello = () => {
   return (
     <div>
-      <div className="Hello">
+      <div className={styles.Hello}>
         <img width="200px" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
+      <div className={styles.Hello}>
         <a
           href="https://electron-react-boilerplate.js.org/"
           target="_blank"
